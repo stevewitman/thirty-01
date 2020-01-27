d1f0762a1fe4a8fb016969ca2e4720d1b8634bd9
Steve Witman <stevewitman@gmail.com>
2020-01-27 08:06:34 -0700

    initial commit

---
+++ apps/app01/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'workspace01-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app01';
+}
